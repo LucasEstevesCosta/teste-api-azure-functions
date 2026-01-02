7323b43e52771f62560391d01cdac2a071ad2ad9
Lucas <lucas.costa@prezensa.com>
2026-01-02 11:59:08 -0300

    fix GetAllTaskItemsService to return list<ShowTaskItemDTO> and not list<TaskItem>

--- teste-api-azure-functions/Application/Services/TaskItems/GetAllTaskItemsService.cs
+++ teste-api-azure-functions/Application/Services/TaskItems/GetAllTaskItemsService.cs
@@ -1,4 +1,5 @@
-﻿using teste_api_azure_functions.Domain.Entities;
+﻿using teste_api_azure_functions.Application.DTOs.TaskItems;
+using teste_api_azure_functions.Domain.Entities;
 using teste_api_azure_functions.Domain.Interfaces;
 
 namespace teste_api_azure_functions.Application.Services.TaskItems;
@@ -12,8 +13,18 @@ public class GetAllTaskItemsService
         _repository = repository;
     }
 
-    public async Task<IReadOnlyList<TaskItem>> ExecuteAsync()
+    public async Task<IReadOnlyList<ShowTaskItemDTO>> ExecuteAsync()
     {
-        return await _repository.GetAllAsync();
+
+        var taskItems = await _repository.GetAllAsync();
+        var result = new List<ShowTaskItemDTO>();
+
+        foreach (TaskItem task in taskItems)
+        {
+            var taskDTO = new ShowTaskItemDTO(task);
+            result.Add(taskDTO);
+        }
+
+        return result;
     }
 }
